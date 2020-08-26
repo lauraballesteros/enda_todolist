7bdc32244971392af53b7ec64373cd20ffc5116b
lauraballesteros <laura_nicollebt@hotmail.com>
2020-08-25 20:57:30 -0500

    i dont fucking know what tha heck is going on,halp

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from todolist.views import showItems, addTodo, deleteTodo, cross_off, uncross
+from todolist.views import showItems, addTodo, deleteTodo, cross_off, uncross, home
 
 urlpatterns = [
+    
     path('todolist/',showItems, name = 'list'),
     path('addTodo/',addTodo),
     path('deleteTodo/<int:todo_id>/',deleteTodo),
     path('cross_off/<int:todo_id>/',cross_off),
     path('uncross/<int:todo_id>/',uncross),
+    path('',home, name = 'home'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
 from.models import todoItem
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+@login_required   
 def showItems (request):
     all_todo_items = todoItem.objects.all()
     return render(request, 'todolist.html',
     {'all_items':all_todo_items})
 
+@login_required   
 def addTodo(request):
     #create a new todo all_items and save it
     #then redirect the browser to /todolist/
@@ -15,17 +21,20 @@ def addTodo(request):
     new_item.save()
     return HttpResponseRedirect('/todolist')
 
+@login_required   
 def deleteTodo(request, todo_id):
     item_to_delete = todoItem.objects.get(id=todo_id)
     item_to_delete.delete()
     return HttpResponseRedirect('/todolist')
 
+@login_required   
 def cross_off(request,todo_id):
     item_to_cross = todoItem.objects.get(id=todo_id)
     item_to_cross.is_completed = True
     item_to_cross.save()
     return HttpResponseRedirect('/todolist')
     
+@login_required   
 def uncross(request,todo_id):
     item_to_uncross = todoItem.objects.get(id=todo_id)
     item_to_uncross.is_completed = False
